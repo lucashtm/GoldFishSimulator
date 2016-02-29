5878c1ae189cbbbe7420a114df3d1f93d1c1b4e6
LucasHMonteiro <lucashtm@id.uff.br>
2016-02-28 22:41:28 -0300

    Adiciona interface controlável ao projeto unity

---
+++ GoldenFishBot/Assets/Scripts/Aquarium.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Aquarium : MonoBehaviour {
+
+	public Color dirty;
+	public Color clean;
+
+	private Renderer rend;
+	private Slider sld;
+
+	void Start () {
+		rend = GetComponentInChildren<Renderer>();
+		sld = GetComponentInChildren<Slider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rend.material.SetColor("_Color" ,Color.Lerp(clean, dirty, sld.value));
+	}
+}

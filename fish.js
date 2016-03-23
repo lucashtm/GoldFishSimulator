2932e4e59334168b0cb3537256a6ca1d4986a65c
LucasHMonteiro <lucashtm@id.uff.br>
2016-03-23 01:57:25 -0300

    remove comentários desnecessários e muda intervalo para 30 min

--- fish.js
+++ fish.js
@@ -1,8 +1,5 @@
 console.log("running");
 
-//var redis = require('redis');
-//var client = redis.createClient();
-
 var Twit = require('twit');
 var config = require('config');
 
@@ -150,7 +147,7 @@ function followed(eventMsg){
   aquarium.followed(eventMsg);
 }
 
-setInterval(updateData, 1000*60);
+setInterval(updateData, 1000*60*30);
 
 function updateData(){
   console.log('called');
@@ -159,76 +156,6 @@ function updateData(){
   aquarium.checkEverything();
 }
 
-// function checkUpdates(){
-//   if(pH < 6.8){
-//     tweetIt(human+' i\'m breathing my own piss '+cont);
-//     cont++;
-//   }
-//
-//   var time = new Date();
-//   var now = time.getHours() + ':' + time.getMinutes();
-//   console.log(now);
-//   for (var i = 0; i < schedule.length; i++) {
-//     if(now >= schedule[i] && !scheduleCheck[i]){
-//       tweetIt(human + ' please feed me');
-//       scheduleCheck[i] = true;
-//       break;
-//     }
-//   }
-// }
-//
-// //function that is triggered when someone follows the user
-// function followed(eventMsg){
-//   console.log('someone followed');
-//   var username = eventMsg.source.screen_name;
-//   if(username !== 'rodneythefish' && username !== 'Lucashtm'){
-//     tweetIt('@'+username+' hope you are not an asshole like my human.');
-//   }else if(username === 'Lucashtm'){
-//     tweetIt('@'+username+' hope now you dont kill me of starvation.');
-//   }
-// }
-//
-// //function that is triggered when someone mentions the user
-// function mentioned(eventMsg){
-//   var fs = require('fs');
-//   var username = eventMsg.user.screen_name;
-//   var tweet = '@'+username;
-//   var tweet_to_reply = eventMsg.id_str;
-//   var json = JSON.stringify(eventMsg, null, 2);
-//
-//   if(eventMsg.in_reply_to_screen_name === "rodneythefish" || eventMsg.text.indexOf('@rodneythefish') != -1){
-//     fs.writeFile('tweet.json', json);
-//     console.log('tweet received');
-//
-//     if(eventMsg.text.indexOf('hello') != -1){
-//       tweet += ' hello.';
-//     }
-//
-//     if((eventMsg.text.indexOf('water') != -1 || eventMsg.text.indexOf('dirty')) != -1){
-//       if(checkDirty()){
-//         tweet += ' everything is alright here, bored as always.';
-//       }else {
-//         tweet += ' im breathing my own piss.';
-//       }
-//     }
-//
-//     if ((eventMsg.text.indexOf('hungry') != -1 || eventMsg.text.indexOf('food') != -1)) {
-//       if(checkFoodTime()){
-//         tweet += ' hungry';
-//       }else {
-//         tweet += ' not hungry';
-//       }
-//     }
-//     if(tweet === '@'+username){
-//       tweet += ' screw yourself.'
-//     }
-//     tweetIt(tweet, tweet_to_reply);
-//   }else{
-//     fs.writeFile('reply.json', json);
-//   }
-// }
-//
-// //This function tweets any text
 function tweetIt(text, in_reply_to_id){
   var tweet = {
     status: text,
@@ -258,24 +185,3 @@ function formatTime(time){
   }
   return '('+month+'/'+day+'/'+year+' '+hour+':'+minute+')';
 }
-//
-// function checkDirty(){
-//   var pH;
-//   client.get("pH", function(err, reply){
-//     pH = parseInt(reply, 10);
-//   });
-//   return pH > 6.8;
-// }
-//
-// function checkFoodTime(){
-//   var schedule = ["8:00", "12:00", "20:00"];
-//   var time = new Date();
-//   var now = time.getHours() + ':' + time.getMinutes();
-//   var value = false;
-//   for (var i = 0; i < schedule.length; i++) {
-//     if(schedule[i] <= now && !scheduleCheck[i]){
-//       value = true;
-//     }
-//   }
-//   return value;
-// }

2eb76130c1a6cd63bb5efb7a2ecbea1b7e5a156e
Ubuntu <ubuntu@ip-172-31-93-173.ec2.internal>
2020-03-25 01:24:52 +0000

    Removes node modules

---
+++ config.js
@@ -0,0 +1,6 @@
+export default {
+  consumer_key: 'rfGuLzSFSJSHp2TydZUK6ReJ0',
+  consumer_secret: 'SNz7Yv6RxsXB8fyqDDDsNzvTAqhejc1Jm7sD3WariKm4j3Yd6o',
+  access_token: '4537391415-DAxDr0PB6lPrs1kACJ8dH1kPkCQKghZYBjjO6Rd',
+  access_token_secret: 'abTjAEH3AVTfgLZ3Kl8nsxYoBThUr0BO8t878mkrzk9d1'
+}
